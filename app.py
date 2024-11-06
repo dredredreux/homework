f6fe8fac5afb66b67c3fc1a854ab3a5bb2b1d344
dredredreux <seva.shestakov.222@gmail.com>
2024-11-06 12:49:09 +0500

    Add files via upload
    создал хранилище

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ class User():
     name: str
     nickname: str
     age: int
+
+
+basedata = {"users":[
+    User(id=1, name='Vsevolod', nickname='seva1337', age=19),
+    User(id=2, name='Alexey', nickname='leha12', age=54)
+],}
